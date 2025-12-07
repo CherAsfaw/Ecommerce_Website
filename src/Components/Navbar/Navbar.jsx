23caa2c186e4a2d9ee98ef05c238dcd8f28e9953
Chernet <cherrefera@gmail.com>
2025-12-07 12:19:57 +0300

    Initial commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      {/* Upper Navbar */}
+      <div>
+        <div>
+          <div>
+            <a href="#"><img src="" alt="" /></a>
+          </div>
+        </div>
+      </div>
+      {/* Lower Navbar */}
+      <div></div>
+    </div>
+  )
+}
+
+export default Navbar
